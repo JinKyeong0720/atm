0f1ec6076f135b7f969da62088e8b55248896370
JinKyeong0720 <jj9jj98664@naver.com>
2023-03-14 13:07:56 +0900

    Create project ATM

---
+++ atm/src/atm/AccountManager.java
@@ -0,0 +1,15 @@
+package atm;
+
+import java.util.ArrayList;
+
+public class AccountManager {
+	
+	
+	private static ArrayList<Account> list = new ArrayList<Account>();
+	
+	// Account에 대한 
+	// Create
+	// Read
+	// Update
+	// Delete 관련 메서드가 AccountManager 안에서 실행
+}
---
+++ atm/src/atm/UserManager.java
@@ -0,0 +1,15 @@
+package atm;
+
+import java.util.ArrayList;
+
+public class UserManager {
+
+	private static ArrayList<User> list = new ArrayList<User>();
+
+	// User에 대한
+	// Create
+	// Read
+	// Update
+	// Delete 관련 메서드가 UserManager 안에서 실행
+	
+}
